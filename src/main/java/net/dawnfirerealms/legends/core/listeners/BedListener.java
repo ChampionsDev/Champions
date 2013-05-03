aae76ab5022d40715123b1dd493848fffc11e3a6
Justin B <justin.d.baldwin91@gmail.com>
2013-05-03 04:54:56 -0400

    Removed config references, fixed formatting.

--- src/main/java/net/dawnfirerealms/legends/core/listeners/BedListener.java
+++ src/main/java/net/dawnfirerealms/legends/core/listeners/BedListener.java
@@ -1,64 +1,49 @@
 package net.dawnfirerealms.legends.core.listeners;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-
 import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
-import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
-import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerBedEnterEvent;
 import org.bukkit.event.player.PlayerBedLeaveEvent;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 
+import java.util.HashMap;
+
 public class BedListener implements Listener{
-    //Todo list for this class:
-    //Add a configuration to look for
-    //Set main with plugin
-    YamlConfiguration config = new YamlConfiguration();
-    plugin;
-    ArrayList<String> players = new ArrayList<String>();
-    HashMap<String, Integer> schedule = new HashMap<String, Integer>();
-    int task;
-    
-    @EventHandler(ignoreCancelled = false)
-        public void onBedEnter(PlayerBedEnterEvent event){
-            final Player player = event.getPlayer();
-                if(player.isSleeping()){
-                if(config.getBoolean("messageonbedenter") == true){
-                    player.sendMessage(ChatColor.RED + config.getString("bedentermessage"));
-                }
-                players.add(player.getName());
-                task = Bukkit.getScheduler().scheduleSyncRepeatingTask(plugin, new Runnable(){
-                    @Override
-                    public void run() {
-                        if(!schedule.containsKey(player.getName())){
-                            schedule.put(player.getName(), task);
-                        }
-                        player.addPotionEffect(new PotionEffect(PotionEffectType.REGENERATION, 20, config.getInt("ampiflier")));
-                    }
-                }, 0L, 20L);
-            }
+    // TODO add config checks
+    Plugin plugin;
+    HashMap<String, Integer> scheduleMap;
+
+    public BedListener(Plugin plugin) {
+        this.plugin = plugin;
+        scheduleMap = new HashMap<>();
     }
     
     @EventHandler(ignoreCancelled = false)
-        public void onBedLeave(PlayerBedLeaveEvent event){
-            Player player = event.getPlayer();
-            if(players.contains(player.getName())){
-                players.remove(player.getName());
-            }
-                if(schedule.containsKey(player.getName())){
-                Bukkit.getScheduler().cancelTask(schedule.get(player.getName()));
-                }else{
-                    Bukkit.getScheduler().cancelTasks(plugin);
+    public void onBedEnter(PlayerBedEnterEvent event){
+        final Player player = event.getPlayer();
+        if(player.isSleeping()){
+            int task = Bukkit.getScheduler().scheduleSyncRepeatingTask(plugin, new Runnable(){
+                @Override
+                public void run() {
+                    player.addPotionEffect(new PotionEffect(PotionEffectType.REGENERATION, 20, 1));
                 }
-                
-                if(config.getBoolean("messageonbedleave") == true){
-                    player.sendMessage(ChatColor.RED + config.getString("bedleavemessage"));
-                }
-            }
+            }, 0L, 20L);
+            scheduleMap.put(player.getName(), task);
+        }
     }
+    
+    @EventHandler(ignoreCancelled = false)
+    public void onBedLeave(PlayerBedLeaveEvent event){
+        Player player = event.getPlayer();
+        if(scheduleMap.containsKey(player.getName())){
+            Bukkit.getScheduler().cancelTask(scheduleMap.get(player.getName()));
+            scheduleMap.remove(player.getName());
+        }else{
+            plugin.getLogger().warning("Could not stop player regeneration!");
+        }
+    }
+}
