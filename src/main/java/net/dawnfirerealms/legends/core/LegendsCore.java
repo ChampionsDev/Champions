1a903ebde7b0c37bb6ccef51742a833655279730
Justin B <justin.d.baldwin91@gmail.com>
2013-05-05 09:02:11 -0400

    Moved DependencyHandler to utils package

--- src/main/java/net/dawnfirerealms/legends/core/LegendsCore.java
+++ src/main/java/net/dawnfirerealms/legends/core/LegendsCore.java
@@ -17,8 +17,7 @@ This file is part of Legends.
 
 package net.dawnfirerealms.legends.core;
 
-import net.dawnfirerealms.legends.core.dependencies.DependencyHandler;
-import net.dawnfirerealms.legends.core.race.RaceHandler;
+import net.dawnfirerealms.legends.core.utils.DependencyHandler;
 import net.dawnfirerealms.legends.core.utils.ConfigHandler;
 import org.bukkit.plugin.java.JavaPlugin;
 import se.ranzdo.bukkit.methodcommand.CommandHandler;
