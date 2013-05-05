0488bcb888fa1a350f1a423d6502accf53e4869a
Justin B <justin.d.baldwin91@gmail.com>
2013-05-05 01:57:03 -0400

    Modified DataManager to use less code and allow for arbitrary DataSources.

--- src/main/java/net/dawnfirerealms/legends/library/database/DataManager.java
+++ src/main/java/net/dawnfirerealms/legends/library/database/DataManager.java
@@ -4,29 +4,39 @@ import net.dawnfirerealms.legends.core.LPlayer;
 import net.dawnfirerealms.legends.library.lclass.LClass;
 import net.dawnfirerealms.legends.library.race.Race;
 
+import java.util.HashMap;
+
 /**
  * @author YoshiGenius
  */
-public abstract class DataManager {
-    
-    public enum DataType {
-        ORACLE, MYSQL, POSTGRE, H2, SQLITE, DAT, YAML;
+public class DataManager {
+    private static DataSource dataSource;
+    private static DataManager instance;
+
+    private static HashMap<String, DataSource> dataSources = new HashMap<>();
+
+    public static DataManager getInstance() {
+        if(instance == null) {
+            instance = new DataManager();
+        }
+        return instance;
+    }
+
+    public static DataSource getDataSource() {
+        return dataSource;
     }
     
-    private static DataType dataType;
-    
-    public static DataType getType() {
-        return dataType;
+    public static void setDataSource(DataSource dataSource) {
+        if(!dataSources.containsKey(dataSource.getDataSourceName())) {
+            dataSources.put(dataSource.getDataSourceName(), dataSource);
+        }
+        DataManager.dataSource = dataSource;
+    }
+
+    public static void registerDataSource(String name, DataSource dataSource) {
+        dataSources.put(name, dataSource);
     }
     
-    public static DataManager getInUse() {
-        return DataManagers.getCurrent();
-    }
-    
-    public abstract LPlayer loadLPlayer(String name);
-    
-    public abstract Race loadRace(String name);
-    
-    public abstract LClass loadLClass(String name);
+
 
 }
