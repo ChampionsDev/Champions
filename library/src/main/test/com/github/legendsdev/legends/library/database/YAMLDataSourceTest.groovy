454b79d78b8d43e942067818f8a2178747951026
Justin B <justin.d.baldwin91@gmail.com>
2013-05-08 09:17:56 -0400

    Finished package renaming process.

--- library/src/main/test/com/github/legendsdev/legends/library/database/YAMLDataSourceTest.groovy
+++ library/src/main/test/com/github/legendsdev/legends/library/database/YAMLDataSourceTest.groovy
@@ -14,9 +14,10 @@ This file is part of Legends.
     You should have received a copy of the GNU General Public License
     along with Legends.  If not, see <http://www.gnu.org/licenses/>.
 */
-package net.dawnfirerealms.legends.library.database
+package com.github.legendsdev.legends.library.database
 
-import net.dawnfirerealms.legends.library.race.Race
+import com.github.legendsdev.legends.library.race.Race
+
 
 /**
  * @author B2OJustin
