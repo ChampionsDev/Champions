560af0fa70dc4715f32a521bbe0d627ffb867847
Justin B <justin.d.baldwin91@gmail.com>
2013-05-15 04:44:31 -0400

    Added configuration and race loading tests to bukkit core.

--- core-bukkit/src/main/java/com/github/legendsdev/legends/bukkit/core/LegendsCore.java
+++ core-bukkit/src/main/java/com/github/legendsdev/legends/bukkit/core/LegendsCore.java
@@ -20,7 +20,10 @@ package com.github.legendsdev.legends.bukkit.core;
 import com.github.legendsdev.legends.bukkit.core.listeners.LegendsListener;
 import com.github.legendsdev.legends.bukkit.core.utils.ConfigHandler;
 import com.github.legendsdev.legends.bukkit.core.utils.DependencyHandler;
+import com.github.legendsdev.legends.library.Configuration;
+import com.github.legendsdev.legends.library.database.DataManager;
 import com.github.legendsdev.legends.library.database.YAMLDataSource;
+import com.github.legendsdev.legends.library.race.RaceHandler;
 import com.github.legendsdev.legends.library.util.JarUtils;
 import org.apache.commons.io.FileUtils;
 import org.bukkit.Bukkit;
@@ -44,8 +47,10 @@ import java.util.logging.Logger;
 public class LegendsCore extends JavaPlugin {
     private static Logger logger = Logger.getLogger(LegendsCore.class.getName());
     private static LegendsCore instance;
+
     private static String JAR_RESOURCE_DIRECTORY = "resources/";
     private static String CONFIG_PATH = "Legends/";
+    private static String MAIN_CONFIG_FILE = "config.yml";
 	
 	private CommandHandler commandHandler;
 	private ConfigHandler configHandler;
@@ -63,15 +68,25 @@ public class LegendsCore extends JavaPlugin {
 
         getServer().getPluginManager().registerEvents(new LegendsListener(), this);
 
-        // Copy default configuration
         try {
+            // Copy default configuration files
             logger.info("Copying default configuration files...");
             int filesCopied = JarUtils.copyDirectoryFromJar(LegendsCore.class, JAR_RESOURCE_DIRECTORY, CONFIG_PATH, false);
             logger.info(String.format("Copied %d files", filesCopied));
+
+            // Load configuration
+            YAMLDataSource yamlDataSource = new YAMLDataSource(CONFIG_PATH);
+            yamlDataSource.loadConfiguration(Configuration.getInstance(), MAIN_CONFIG_FILE);
         } catch (IOException e) {
             logger.severe("Could not write default configuration files to disk.");
             e.printStackTrace();
         }
+
+        //Initialize data management
+        DataManager.init(Configuration.getInstance());
+
+        // Testing
+        RaceHandler.getInstance().load("Test");
 
         logger.info("Legends successfully enabled!");
 	}
