2b76b821dbc14a7089b746bdc5eeedfab9b3cb18
Justin B <justin.d.baldwin91@gmail.com>
2013-05-22 14:56:52 -0400

    Added cancel methods to ChampionsEvent

--- library/src/main/java/com/github/championsdev/champions/library/event/ChampionsEvent.java
+++ library/src/main/java/com/github/championsdev/champions/library/event/ChampionsEvent.java
@@ -6,5 +6,13 @@ package com.github.championsdev.champions.library.event;
  * @author YoshiGenius
  */
 public class ChampionsEvent {
+    private boolean isCancelled = false;
 
+    public void setCancelled(boolean isCancelled) {
+        this.isCancelled = isCancelled;
+    }
+
+    public boolean isCancelled() {
+        return isCancelled;
+    }
 }
