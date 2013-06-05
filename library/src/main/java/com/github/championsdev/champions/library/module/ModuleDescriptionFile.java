d7b3a0ab7c5b464e31dfde954877dc7a4583812d
Justin B <justin.d.baldwin91@gmail.com>
2013-06-05 02:26:42 -0400

    Copyright notices and @author tag consistency.

--- library/src/main/java/com/github/championsdev/champions/library/module/ModuleDescriptionFile.java
+++ library/src/main/java/com/github/championsdev/champions/library/module/ModuleDescriptionFile.java
@@ -1,3 +1,20 @@
+/*
+ * This file is part of Champions.
+ *
+ *     Champions is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     Champions is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with Champions.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.github.championsdev.champions.library.module;
 
 import org.yaml.snakeyaml.Yaml;
@@ -13,8 +30,6 @@ import java.util.Map;
 
 /**
  * @author YoshiGenius
- *         Date: 23/05/13
- *         Time: 7:14 PM
  */
 public final class ModuleDescriptionFile {
     private static final Yaml yaml = new Yaml(new SafeConstructor());
