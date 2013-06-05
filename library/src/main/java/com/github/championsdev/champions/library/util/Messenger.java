e439b390ab726a0a95d906570f2980b3b20abc77
Justin B <justin.d.baldwin91@gmail.com>
2013-06-05 02:43:36 -0400

    GPL Notice Consistency.

--- library/src/main/java/com/github/championsdev/champions/library/util/Messenger.java
+++ library/src/main/java/com/github/championsdev/champions/library/util/Messenger.java
@@ -1,4 +1,4 @@
-/*
+/*******************************************************************************
  * This file is part of Champions.
  *
  *     Champions is free software: you can redistribute it and/or modify
@@ -13,7 +13,7 @@
  *
  *     You should have received a copy of the GNU General Public License
  *     along with Champions.  If not, see <http://www.gnu.org/licenses/>.
- */
+ ******************************************************************************/
 
 package com.github.championsdev.champions.library.util;
 
