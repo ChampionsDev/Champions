7758feb68be428877c6baaf9bc39974ed2a0ba9f
Justin B <justin.d.baldwin91@gmail.com>
2013-06-11 10:50:06 -0400

    Revert "Return the CPlayer if it exists from the source."
    This reverts commit 61ac69770f2394b40a6b97d58f33fda9e3dfe986.

--- library/src/main/java/com/github/championsdev/champions/library/cplayer/CPlayerHandler.java
+++ library/src/main/java/com/github/championsdev/champions/library/cplayer/CPlayerHandler.java
@@ -44,7 +44,6 @@ public class CPlayerHandler extends BasicHandler<CPlayer> {
             if(lPlayer != null) {
                 register(id, lPlayer);
                 logger.info("Loaded player '" + id + "' from cache");
-                return lPlayer;
             }
         }
 
@@ -54,7 +53,6 @@ public class CPlayerHandler extends BasicHandler<CPlayer> {
             if(lPlayer != null) {
                 register(id, lPlayer);
                 logger.info("Loaded player data for '" + id + "' from database");
-                return lPlayer;
             }
         }
 
