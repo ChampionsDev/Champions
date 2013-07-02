c79e0fbb9122a27bee00ead923cba35e1bbf2c7a
B2OJustin <justin.d.baldwin91@gmail.com>
2013-07-02 01:18:21 -0400

    Re-implemented convenience constructors for Descriptor

--- library/src/main/java/com/github/championsdev/champions/library/misc/Descriptor.java
+++ library/src/main/java/com/github/championsdev/champions/library/misc/Descriptor.java
@@ -21,6 +21,7 @@ import com.github.championsdev.champions.library.exceptions.InvalidDescriptorExc
 import org.yaml.snakeyaml.Yaml;
 
 import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -28,13 +29,13 @@ import java.util.Map;
  * @author B2OJustin
  */
 public class Descriptor {
-    private String name = null;
-    private String main = null;
-    private String version = null;
-    private String description = null;
-    private List<String> authors = null;
-    private String website = null;
-    private String[] platforms = new String[]{};
+    private String name;
+    private String main;
+    private String version;
+    private String description;
+    private List<String> authors;
+    private String website;
+    private String[] platforms;
 
     public Descriptor(final String name, final String version, final String mainClass, final String[] supportedPlatforms) {
         this.name = name;
@@ -43,12 +44,91 @@ public class Descriptor {
         platforms = supportedPlatforms;
     }
 
-    //TODO
     public Descriptor(InputStream inputStream) throws InvalidDescriptorException {
         Yaml yaml = new Yaml();
         Object object = yaml.load(inputStream);
         if(object == null || !(object instanceof Map)) {
             throw new InvalidDescriptorException("Unable to process descriptor input stream");
+        }
+        loadMap((Map<String, ?>) object);
+    }
+
+    public Descriptor(Map<String, ?> map) throws InvalidDescriptorException {
+        loadMap(map);
+    }
+
+    private void loadMap(Map<String, ?> map) throws InvalidDescriptorException {
+        if (map == null) return;
+        try {
+            name = map.get("name").toString();
+
+            if (!name.matches("^[A-Za-z0-9 _.-]+$")) {
+                return;
+            }
+        } catch (NullPointerException ex) {
+            return;
+        } catch (ClassCastException ex) {
+            return;
+        }
+
+        try {
+            version = map.get("version").toString();
+        } catch (NullPointerException ex) {
+            return;
+        } catch (ClassCastException ex) {
+            return;
+        }
+
+        try {
+            main = map.get("main").toString();
+            if (main.startsWith("com.github.championsdev.champions.")) {
+                return;
+            }
+        } catch (NullPointerException ex) {
+            return;
+        } catch (ClassCastException ex) {
+            return;
+        }
+        try {
+            Object obj = map.get("platforms");
+            if (obj != null && obj instanceof String[]) {
+                platforms = (String[]) obj;
+            } else {
+                return;
+            }
+        } catch (NullPointerException ex) {
+            return;
+        }
+
+        if (map.get("website") != null) {
+            website = map.get("website").toString();
+        }
+
+        if (map.get("description") != null) {
+            description = map.get("description").toString();
+        }
+
+        if (map.get("authors") != null) {
+            ArrayList<String> authorsBuilder = new ArrayList<String>();
+            if (map.get("author") != null) {
+                authorsBuilder.add(map.get("author").toString());
+            }
+            try {
+                for (Object o : (Iterable<?>) map.get("authors")) {
+                    authorsBuilder.add(o.toString());
+                }
+            } catch (ClassCastException ex) {
+                return;
+            } catch (NullPointerException ex) {
+                return;
+            }
+            authors = authorsBuilder;
+        } else if (map.get("author") != null) {
+            ArrayList<String> authorz = new ArrayList<String>();
+            authorz.add(map.get("author").toString());
+            authors = authorz;
+        } else {
+            authors = new ArrayList<String>();
         }
     }
 
